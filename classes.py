64396a51773c60deebde4a50efea8f2444d25199
Garling <rgmuffin1108@gmail.com>
2020-11-05 14:03:59 +0000

    Initial commit

---
+++ classes.py
@@ -0,0 +1,49 @@
+class Animal():
+  size = "unknown"
+  is_feed = False
+  
+  def set_size(self, newsize):
+    self.size = newsize
+  
+  def get_size(self):
+    print(self.size)
+  
+  def feed(self):
+    self.is_feed = True 
+  
+  def need_to_feed(self):
+    if self.is_feed == False:
+      print("Yes")
+    else:
+      print("No")  
+  
+    
+  
+class Cat(Animal):
+  id = 0
+  def __init__(self):
+    print("Cat was created")
+    self.id += 1
+
+  def feed(self, food):
+    if food == "milk":
+      self.is_feed = True
+      print("Meow")
+    else:
+      print("Cat's eat only milk!")
+ 
+
+class Dog(Animal):
+  id = 0
+  def __init__(self):
+    print("Dog was created")
+    self.id += 1
+  
+  def feed(self, food):
+    if food == "meat":
+      self.is_feed = True
+      print("Woof")
+    else:
+      print("Dog's eat only meat!")
+
+      
---
+++ main.py
@@ -0,0 +1,12 @@
+from classes import Cat, Dog
+
+cat = Cat()
+cat.set_size("small") # Big, meduim or small
+cat.get_size()
+cat.feed("milk") # Cat's eat only milk
+cat.need_to_feed()
+dog = Dog()
+dog.set_size("Big")
+dog.get_size()
+dog.feed("milk") # Cat's eat only meat
+dog.need_to_feed()
